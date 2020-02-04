87bb28027451bd2ba0d4e7f61e241d9b40668317
zhi_shui <zhi_shui@hunliji.com>
2020-02-04 18:21:52 +0800

    init

--- utils/tools/readyLaunch.js
+++ utils/tools/readyLaunch.js
@@ -3,16 +3,12 @@ const { checkServerVer } = require('./checkVersion')
 const { warning, info } = require('./tip')
 
 module.exports = async function(config) {
-  const { onOk, checkVersion, serverName } = config || {}
+  const { onOk, serverName } = config || {}
   info('正在启动,请稍等......')
-  if (!checkVersion) {
-    warning('不建议配置checkVersion为false，因为这样将无法检测你本地项目配置是否更新到最新')
-  } else {
-    const serverVersion = await checkServerVer(serverName)
-    if (serverVersion) {
-      const { currentVer, latestVer } = serverVersion || {}
-      warning(`检测到工程配置有更新，最新版本为 ${latestVer} ,当前版本为 ${currentVer}`)
-    }
+  const serverVersion = await checkServerVer(serverName)
+  if (serverVersion) {
+    const { currentVer, latestVer } = serverVersion || {}
+    info(`react-admin最新版本为 ${latestVer} ,当前版本为 ${currentVer}`)
   }
   onOk && onOk()
 }
