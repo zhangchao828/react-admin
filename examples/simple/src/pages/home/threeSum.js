bc44d293fccaf10297fb68103671577e9e61ffc4
zhangchao <350346750@qq.com>
2022-03-01 15:54:11 +0800

    init

--- examples/simple/src/pages/home/threeSum.js
+++ examples/simple/src/pages/home/threeSum.js
@@ -1,15 +1,11 @@
 function threeSum(nums) {
   const result = []
-  //从小到大排列
   nums.sort((a, b) => a - b)
   for (let i = 0; i < nums.length; i++) {
-    // 跳过重复数字
-    if (i && nums[i] === nums[i - 1]) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
       continue
     }
-    //左指针
     let left = i + 1
-    //右指针
     let right = nums.length - 1
     while (left < right) {
       const sum = nums[i] + nums[left] + nums[right]
@@ -18,17 +14,13 @@ function threeSum(nums) {
       } else if (sum < 0) {
         left++
       } else {
-        //值=0 加入结果数组
         result.push([nums[i], nums[left], nums[right]])
         left++
         right--
-        //继续移动指针寻找
-        // 跳过重复数字
-        while (nums[left] === nums[left - 1]) {
+        if (nums[left] === nums[left - 1]) {
           left++
         }
-        // 跳过重复数字
-        while (nums[right] === nums[right + 1]) {
+        if (nums[right] === nums[right + 1]) {
           right--
         }
       }
