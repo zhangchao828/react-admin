b94332936527a2c9178dfa30fadc09d2ea2a1398
zhangchao <350346750@qq.com>
2022-04-09 17:33:17 +0800

    vite example

--- examples/file-router/admin.config.js
+++ examples/file-router/admin.config.js
@@ -1,10 +1,6 @@
 const defineConfig = require('@zc/admin/define')
 
 module.exports = defineConfig({
-  vite: false,
-  https: false,
-  port: 3006,
-  eslint: false,
   webpack: {
     builtInExternals: false,
   },
--- examples/simple/src/pages/home/palindrome.js
+++
@@ -1,8 +0,0 @@
-function palindrome(str) {
-  str = str.toLowerCase().replace(/[\W_]/, '')
-  return str === str.split('').reverse().join('')
-}
-
-export default function test() {
-  console.log(palindrome('123321'))
-}
