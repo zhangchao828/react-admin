bb630e98babc436f4c7adbc1950a0a8709835acb
张超 <zhangchao@mistong.com>
2025-04-28 17:21:39 +0800

    eslint

--- packages/admin/src/mobx/index.d.ts
+++ packages/admin/src/mobx/index.d.ts
@@ -1,2 +1,2 @@
-export { flow, makeAutoObservable, configure, toJS, runInAction } from 'mobx'
+export { makeAutoObservable, toJS, runInAction } from 'mobx'
 export { observer } from 'mobx-react-lite'
